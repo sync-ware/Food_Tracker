33d706bc169834b2debc61732880e1cc987fcb89
Saahil <saahilms@gmail.com>
2019-03-06 16:22:00 +0000

    Recipe details are now viewable when recipe is clicked Added edit image asset Added layout elements to view recipe activity Added cardview dependency

--- app/src/main/java/com/csed/foodtracker/ViewRecipeActicity.java
+++ app/src/main/java/com/csed/foodtracker/ViewRecipeActicity.java
@@ -3,11 +3,15 @@ package com.csed.foodtracker;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class ViewRecipeActicity extends AppCompatActivity {
+
+    private Recipe recipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,19 @@ public class ViewRecipeActicity extends AppCompatActivity {
         setContentView(R.layout.activity_view_recipe_acticity);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        recipe = (Recipe) getIntent().getSerializableExtra("recipe");
+
+        TextInputEditText recipeName = (TextInputEditText) findViewById(R.id.text_name);
+        recipeName.setText(recipe.getName());
+        TextInputEditText recipeDesc = (TextInputEditText) findViewById(R.id.text_desc);
+        recipeDesc.setText(recipe.getDescription());
+        EditText recipePrepTime = (EditText) findViewById(R.id.text_preptime);
+        recipePrepTime.setText(recipe.getPrepTime());
+        EditText recipeCalories = (EditText) findViewById(R.id.text_calories);
+        recipeCalories.setText(Integer.toString(recipe.getCalories()));
+        TextInputEditText recipeUrl = (TextInputEditText) findViewById(R.id.text_url);
+        recipeUrl.setText(recipe.getUrl());
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
