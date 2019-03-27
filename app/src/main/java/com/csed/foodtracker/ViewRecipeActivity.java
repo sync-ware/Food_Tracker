79ef167839526525ccb8e35ed56e5436c83fd6d9
Saahil <saahilms@gmail.com>
2019-03-27 12:43:21 +0000

    Can now delete a Recipe

--- app/src/main/java/com/csed/foodtracker/ViewRecipeActivity.java
+++ app/src/main/java/com/csed/foodtracker/ViewRecipeActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -137,6 +139,35 @@ public class ViewRecipeActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.recipe_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        switch(item.getItemId()){
+            //Deletes Recipe from Ingredients
+            case R.id.action_delete:
+
+                mDb.execSQL("DELETE FROM Recipes WHERE recipe_id = " + recipe.getId() + ";");
+                mDb.execSQL("DELETE FROM RecipeIngredients WHERE recipe_id = " + recipe.getId() + ";");
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+
     }
 
 }
